d487f570f69c46222af221d100889bf49846a4df
rjporosh <porosh19940423@gmail.com>
2019-01-31 16:21:28 +0600

    First Commit

---
+++ BrowserTest/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace BrowserTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtGoto = new System.Windows.Forms.TextBox();
+            this.btnGoto = new System.Windows.Forms.Button();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.btnGoNext = new System.Windows.Forms.Button();
+            this.btnGoHome = new System.Windows.Forms.Button();
+            this.btnSubscribe = new System.Windows.Forms.Button();
+            this.btnGoBack = new System.Windows.Forms.Button();
+            this.webBrowser = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // txtGoto
+            // 
+            this.txtGoto.Location = new System.Drawing.Point(109, 42);
+            this.txtGoto.Name = "txtGoto";
+            this.txtGoto.Size = new System.Drawing.Size(571, 20);
+            this.txtGoto.TabIndex = 0;
+            // 
+            // btnGoto
+            // 
+            this.btnGoto.Location = new System.Drawing.Point(28, 21);
+            this.btnGoto.Name = "btnGoto";
+            this.btnGoto.Size = new System.Drawing.Size(75, 58);
+            this.btnGoto.TabIndex = 1;
+            this.btnGoto.Text = "Go to";
+            this.btnGoto.UseVisualStyleBackColor = true;
+            this.btnGoto.Click += new System.EventHandler(this.btnGoto_Click);
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(686, 33);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(77, 37);
+            this.btnRefresh.TabIndex = 1;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // btnGoNext
+            // 
+            this.btnGoNext.Location = new System.Drawing.Point(539, 74);
+            this.btnGoNext.Name = "btnGoNext";
+            this.btnGoNext.Size = new System.Drawing.Size(118, 41);
+            this.btnGoNext.TabIndex = 1;
+            this.btnGoNext.Text = "Go Next";
+            this.btnGoNext.UseVisualStyleBackColor = true;
+            this.btnGoNext.Click += new System.EventHandler(this.btnGoNext_Click);
+            // 
+            // btnGoHome
+            // 
+            this.btnGoHome.Location = new System.Drawing.Point(241, 84);
+            this.btnGoHome.Name = "btnGoHome";
+            this.btnGoHome.Size = new System.Drawing.Size(76, 35);
+            this.btnGoHome.TabIndex = 1;
+            this.btnGoHome.Text = "Go Home";
+            this.btnGoHome.UseVisualStyleBackColor = true;
+            this.btnGoHome.Click += new System.EventHandler(this.btnGoHome_Click);
+            // 
+            // btnSubscribe
+            // 
+            this.btnSubscribe.Location = new System.Drawing.Point(336, 84);
+            this.btnSubscribe.Name = "btnSubscribe";
+            this.btnSubscribe.Size = new System.Drawing.Size(75, 23);
+            this.btnSubscribe.TabIndex = 1;
+            this.btnSubscribe.Text = "Subscribe";
+            this.btnSubscribe.UseVisualStyleBackColor = true;
+            this.btnSubscribe.Click += new System.EventHandler(this.btnSubscribe_Click);
+            // 
+            // btnGoBack
+            // 
+            this.btnGoBack.Location = new System.Drawing.Point(121, 74);
+            this.btnGoBack.Name = "btnGoBack";
+            this.btnGoBack.Size = new System.Drawing.Size(91, 45);
+            this.btnGoBack.TabIndex = 1;
+            this.btnGoBack.Text = "Go back";
+            this.btnGoBack.UseVisualStyleBackColor = true;
+            this.btnGoBack.Click += new System.EventHandler(this.btnGoBack_Click);
+            // 
+            // webBrowser
+            // 
+            this.webBrowser.Location = new System.Drawing.Point(12, 125);
+            this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.Size = new System.Drawing.Size(810, 355);
+            this.webBrowser.TabIndex = 2;
+            this.webBrowser.Url = new System.Uri("http://www.youtube.com/rjporosh", System.UriKind.Absolute);
+            this.webBrowser.Navigated += new System.Windows.Forms.WebBrowserNavigatedEventHandler(this.webBrowser_Navigated);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(834, 501);
+            this.Controls.Add(this.webBrowser);
+            this.Controls.Add(this.btnGoBack);
+            this.Controls.Add(this.btnSubscribe);
+            this.Controls.Add(this.btnGoHome);
+            this.Controls.Add(this.btnGoNext);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.btnGoto);
+            this.Controls.Add(this.txtGoto);
+            this.Name = "Form1";
+            this.Text = "Browser";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtGoto;
+        private System.Windows.Forms.Button btnGoto;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.Button btnGoNext;
+        private System.Windows.Forms.Button btnGoHome;
+        private System.Windows.Forms.Button btnSubscribe;
+        private System.Windows.Forms.Button btnGoBack;
+        private System.Windows.Forms.WebBrowser webBrowser;
+    }
+}
+
---
+++ BrowserTest/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BrowserTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGoto_Click(object sender, EventArgs e)
+        {
+            webBrowser.Navigate(txtGoto.Text);
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            webBrowser.Refresh();
+        }
+
+        private void btnGoHome_Click(object sender, EventArgs e)
+        {
+            webBrowser.GoHome();
+        }
+
+        private void btnGoBack_Click(object sender, EventArgs e)
+        {
+            webBrowser.GoBack();
+        }
+
+        private void btnGoNext_Click(object sender, EventArgs e)
+        {
+            webBrowser.GoForward();
+        }
+
+        private void webBrowser_Navigated(object sender, WebBrowserNavigatedEventArgs e)
+        {
+            txtGoto.Text = webBrowser.Url.ToString();
+        }
+
+        private void btnSubscribe_Click(object sender, EventArgs e)
+        {
+            webBrowser.Document.GetElementById("search-icon-legacy").InvokeMember("yt-action");
+            //webBrowser.Document.GetElementById("button").InvokeMember("Click");
+        }
+    }
+}
